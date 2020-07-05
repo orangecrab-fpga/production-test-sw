8bdbb3cebebe202c9eac55fd647236d4c3c7a5f6
Greg Davill <greg.davill@gmail.com>
2020-07-05 15:29:20 +0930

    clean up battery test names

--- fw/main.c
+++ fw/main.c
@@ -160,33 +160,33 @@ int main(int i, char **c)
 	printf("ADC-VBAT=%ld\n", adc_read_channel(15));
 	printf("Test:ADC, Finish\n");
 
-
+	printf("Test:BATT, Start\n");
 	/* Battery test */
-
-	/* Enable current sink */
+	/* Enable current sink + Dummy Battery, takes ~20ms for charger to react */
+	printf("BATT: Enable Current Sink\n");
+	printf("BATT: Enable Dummy Battery\n");
 	mcp23s08_write(0x9, 1 << 6 | 1 << 7);
-	printf("bat_en=1,sink_en=1: %ld (vbat)\n", adc_read_channel(15));
 	for(int i = 0; i < 20; i++){
 		msleep(1);
-		printf("Bat: %ld (vbat)\n", adc_read_channel(15));
+		printf("ADC-VBAT=%ld\n", adc_read_channel(15));
 	}
 
+	printf("BATT: Remove Dummy Battery\n");
 	/* Enable current sink */
 	mcp23s08_write(0x9, 1 << 7);
-	printf("sink_en=1: %ld (vbat)\n", adc_read_channel(15));
 	for(int i = 0; i < 20; i++){
 		msleep(1);
-		printf("Bat: %ld (vbat)\n", adc_read_channel(15));
+		printf("ADC-VBAT=%ld\n", adc_read_channel(15));
 	}
 
 	/* Enable current sink */
+	printf("BATT: Remove Current Sink\n");
 	mcp23s08_write(0x9, 0);
-	printf("vbat: %ld (vbat)\n", adc_read_channel(15));
 	for(int i = 0; i < 20; i++){
 		msleep(1);
-		printf("Bat: %ld (vbat)\n", adc_read_channel(15));
+		printf("ADC-VBAT=%ld\n", adc_read_channel(15));
 	}
-
+	printf("Test:BATT, Finish\n");
 
 	/* Test of LED GPIO */
 	uint8_t led_gpio_patterns[] = {0x0, 0x1, 0x2, 0x4, 0x7};
@@ -203,9 +203,10 @@ int main(int i, char **c)
 		printf("{%01X:%01X}\n",out_pattern, read_pattern);
 	}
 
-
 	/*  */
-	printf("\nTests Complete\n");
+	printf("Test:DONE, Finish\n");
+
+	while(1){}
 
 	return 0;
 }
